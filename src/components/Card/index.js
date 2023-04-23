2f4bbb6bd9e89ef826463a824e6db89723303931
MarilynCat <polinamakarova97@gmail.com>
2023-04-23 22:04:42 +0300

    хуки, бек, корзина

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,29 +1,31 @@
+import React from 'react';
 import styles from './Card.module.scss'
 
-function Index(props) {
-    const onClickButton = () => {
-        alert(props.title)
-    }
+function Card({onFavorite, imageUrl, title, price, onPlus}) {
+const [isAdded, setIsAdded] = React.useState(false);
+
+const onClickPlus = () => {
+    onPlus({imageUrl, title, price});
+    setIsAdded(!isAdded);
+}
 
     return(
         <div className={styles.card}>
-            <div className={styles.favorite}>
+            <div className={styles.favorite} onClick={onFavorite}>
                 <img src="/img/heart-unliked.svg" alt="Unliked"/>
             </div>
 
-            <img width={133} height={112} src={props.imageUrl} alt="Sneakers"/>
-            <h5>{props.title}</h5>
+            <img width={133} height={112} src={imageUrl} alt="Sneakers"/>
+            <h5>{title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Цена:</span>
-                    <b>{props.price} руб.</b>
+                    <b>{price} руб.</b>
                 </div>
-                <button className="button" onClick={() => onClickButton()}>
-                    <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
-                </button>
+                    <img className={styles.plus} onClick={onClickPlus} src={isAdded ? "/img/btn-cheked.svg" : "/img/btn-plus.svg"} alt="Plus"/>
             </div>
         </div>
     );
 }
 
-export default Index;
+export default Card;
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,31 +1,26 @@
-function Drawer() {
+function Drawer({onClose, items = []}) {
     return(
-        <div style={{ display: 'none' }} className="overlay">
+        <div className="overlay">
         <div className="drawer">
-            <h2 className="mb-30 d-flex justify-between">Корзина <img className="cu-p" src="/img/btn-remove.svg" alt="Remove"/></h2>
+            <h2 className="mb-30 d-flex justify-between">
+                Корзина <img onClick={onClose} className="cu-p" src="/img/btn-remove.svg" alt="Remove"/></h2>
 
-
-            <div className="cartItem d-flex align-center mb-20">
-                <div style={{ backgroundImage: 'url(img/sneakers/1.jpg)' }} className="cartItemImg"></div>
-
-                <div className="mr-20 flex">
-                    <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
-                    <b>12 999 руб.</b>
-                </div>
-                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
-            </div>
-
-            <div className="cartItem d-flex align-center mb-20">
-                <div style={{ backgroundImage: 'url(img/sneakers/1.jpg)' }} className="cartItemImg"></div>
-
-                <div className="mr-20 flex">
-                    <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
-                    <b>12 999 руб.</b>
-                </div>
-                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
-            </div>
 
             <div className="items">
+                {
+                   items.map((obj) => (
+                       <div className="cartItem d-flex align-center mb-20">
+                           <div style={{ backgroundImage:  `url(${obj.imageUrl})` }} className="cartItemImg"></div>
+
+                           <div className="mr-20 flex">
+                               <p className="mb-5">{obj.title}</p>
+                               <b>{obj.price} руб.</b>
+                           </div>
+                           <img className="removeBtn" src="/img/btn-remove.svg" alt="Close"/>
+                       </div>
+                   ))}
+            </div>
+
                 <div className="cartTotalBlock">
                     <ul>
                         <li>
@@ -42,7 +37,7 @@ function Drawer() {
                     <button className="greenButton">Оформить заказ <img src="/img/arrow.svg" alt="Arrow"/></button>
                 </div>
             </div>
-        </div>
+
         </div>
     );
 }
